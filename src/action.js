653a78cdef5c556d6333172f4bfdff2d51c48b98
Shane Bonham <bonham@ministrycentered.com>
2025-03-18 10:50:21 -0700

    Add some more logging
    - Human-authored PR assigned to human
    - Bot-authored PR not assigned
    - Already assigned PR not re-assigned

--- src/action.js
+++ src/action.js
@@ -15,14 +15,18 @@ async function run() {
   console.log(`assignees: ${JSON.stringify(assignees, undefined, 2)}`)
   console.log(`author: ${JSON.stringify(author, undefined, 2)}`)
 
-  if (
-    (!assignees || assignees.length === 0) &&
-    (author.is_bot === false || (author.type && author.type !== 'Bot'))
-  ) {
-    await client.request(
-      `POST /repos/${owner}/${repo}/issues/${issue_number}/assignees`,
-      { owner, repo, issue_number, assignees: [author.login] }
-    )
+  if (!assignees || assignees.length === 0) {
+    if (author.is_bot === false || (author.type && author.type !== 'Bot')) {
+      console.log(`PR was created by a human (${author.login}). Assigning them.`)
+      await client.request(
+        `POST /repos/${owner}/${repo}/issues/${issue_number}/assignees`,
+        { owner, repo, issue_number, assignees: [author.login] }
+      )
+    } else {
+      console.log(`PR was created by a bot (${author.login}). No assignee added.`)
+    }
+  } else {
+    console.log('PR already has assignees. No changes made.')
   }
 }
 
